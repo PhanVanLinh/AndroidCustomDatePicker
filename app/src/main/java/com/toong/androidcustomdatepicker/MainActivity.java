2f44827eb9b073d635909d3529e2852e6768c3d7
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-09-08 15:57:45 +0700

    first commit

---
+++ app/src/main/java/com/toong/androidcustomdatepicker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.toong.androidcustomdatepicker;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                    int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                Log.i(TAG, "selected time "+myCalendar.getTimeInMillis());
+            }
+        };
+
+        findViewById(R.id.open_date_picker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog a = new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH));
+//                a.getDatePicker().setMinDate(24224532);
+                a.show();
+            }
+        });
+    }
+}
